0dbbde86a5a5a393fcb15ce8130c4433430eab67
mehedi <mehedi25122003@gmail.com>
2025-03-25 11:21:24 +0600

    errorpage and navbar setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Navbar from './Navbar.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import ErrorPage from './ErrorPage.jsx'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Navbar></Navbar>,
+    errorElement: <ErrorPage></ErrorPage>
   },
 ]);
 createRoot(document.getElementById('root')).render(
